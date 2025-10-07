fc239863d5453b5a8c5010fb9567adda70658f22
Leonardo <leonardobarbonabreu@gmail.com>
2025-10-07 08:55:29 -0300

    Criando Estrutura Projeto

---
+++ src/Source/MODEL/LivroModel.java
@@ -0,0 +1,14 @@
+package projetoLivraria.MODEL;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Leonardo José
+ */
+public class LivroModel {
+    
+}
